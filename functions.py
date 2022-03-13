77b49dd189401f98f30815d8618c80ec87a3196b
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-13 17:45:46 +0000

    create function to train the bot

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import os
 import speech_recognition as sr
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
 
 # inglês
 ENGLISH = 'espeak -s 120 -v us-mbrola-2'
@@ -14,6 +16,17 @@ def say_english(phrase):
 
 def say_portuguese(phrase):
     os.system(PORTUGUESE + ' "'+phrase+'"')
+
+
+def builtTrainedBot():
+    bot = ChatBot('JARVS')
+    trainer = ListTrainer(bot)
+
+    for _file in os.listdir('chats'):
+        fileLines = open('chats/' + _file, 'r').readlines()
+        trainer.train(fileLines)
+
+    return bot
 
 
 def listenMe():
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # salvar dependências: pip freeze > packages
 # instalar dependências: pip install -r packages
 
-from functions import listenMe
+from functions import listenMe, builtTrainedBot
 
 # say_english('Hello world!')
 # say_portuguese('olá tudo bem')
 
 
 def start():
+    bot = builtTrainedBot()
     phrase = listenMe()
     print('you: ', phrase)
 
